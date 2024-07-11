e621fda72a3c4e553fbeb6e05daa6af6b8807d3f
MrKyrylo <138040487+MrKyrylo@users.noreply.github.com>
2024-07-11 18:11:55 +0300

    1

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -6,37 +6,40 @@ import searchMoviesByName from "../../functions/searchMoviesByName";
 import MovieList from "../../components/MovieList/MovieList";
 
 export default function MoviesPage() {
-  const [keyWord, setKeyWord] = useState("");
   const [foundMovies, setFoundMovies] = useState([]);
-
   const [params, setParams] = useSearchParams();
+
+  const keyWord = useMemo(() => params.get("searchword") ?? "", [params]);
+
   useEffect(() => {
     async function makeFetch() {
       if (params.get("searchword")) {
         try {
-          const responce = await searchMoviesByName(params.get("searchword"));
-          setFoundMovies(responce.data.results);
+          const response = await searchMoviesByName(params.get("searchword"));
+          setFoundMovies(response.data.results);
         } catch (error) {
-          console.log(console.log(error));
+          console.log(error);
         }
       }
     }
     makeFetch();
   }, [params]);
 
-  function handleSubmit(values) {
-    if (values.searchWord.trim().length > 0) {
-      setKeyWord(values.searchWord);
-      params.set("searchword", values.searchWord);
-      setParams(params);
-    }
-  }
+  const handleSubmit = useMemo(() => {
+    return (values) => {
+      if (values.searchWord.trim().length > 0) {
+        setParams(new URLSearchParams({ searchword: values.searchWord }));
+      } else {
+        setParams();
+      }
+    };
+  }, [setParams]);
 
   return (
     <>
       <SearchBar
         handleSubmit={handleSubmit}
-        value={params.get("searchword") ?? ""}
+        value={keyWord}
       />
       {foundMovies.length > 0 && <MovieList Movies={foundMovies} />}
     </>
